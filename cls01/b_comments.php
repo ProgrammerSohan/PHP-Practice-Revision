e5cf81793bbd1370648d6f1a0f011fae03ce233d
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-02 23:15:50 +0600

    02_comments of php

---
+++ cls01/b_comments.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Revision</title>
+</head>
+<body>
+
+<?php 
+echo "<h2>PHP Comments</h2> <br>";
+ // this is a single-line comment
+
+ #this is also a single-line comment
+
+ /*
+ this is a multi-line comment
+ */
+echo "<h3>Using comments to ignore parts of a code line:</h3>";
+ $x = 5 /*+15*/+5;
+ echo $x;
+
+?>
+    
+</body>
+</html>
