d5addf7783ee18b533aa728473add0eed8147699
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-07-02 23:07:12 +0600

    01_syntax

---
+++ cls01/a_syntax.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Revision</title>
+</head>
+<body>
+
+<?php 
+   // echo "Hello Sohan!";
+   //php case sensitivity, all three echo statements below are equal & legal
+   ECHO "Hello Sohan1!<br>";
+   echo "Hello Sohan2!<br>";
+   EcHo "Hello Sohan3!<br>";
+   echo "<h3>All variable names are case-sensitive</h3><br>";
+   $color = "red";
+   echo "My car is " . $color . "<br>";
+   echo "My house is " . $COLOR . "<br>";
+   echo "My boat is " . $coLOR . "<br>";
+
+?>
+    
+</body>
+</html>
